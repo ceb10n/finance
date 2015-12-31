ffb54bc9df1253222ce6961b8b5e8018f4f1d181
Rafael de Oliveira Marques <rafaelomarques@gmail.com>
2015-12-31 18:41:47 -0200

    removed exception

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.AspNet.Mvc;
 
 namespace finance.Controllers
@@ -7,7 +6,6 @@ namespace finance.Controllers
     {
         public IActionResult Index()
         {
-            throw new Exception();
             return View();
         }
     }
